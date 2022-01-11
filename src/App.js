2eb56e9ba29e268d0cdbf7e64871bd221f8d9b2f
ilyesnb <ilyesnabi9@gmail.com>
2022-01-11 17:20:25 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -2,129 +2,92 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>ReactJS Form with Validation and layout</h1>
-      <div class="row g-3">
-        <div class="col">
-          <label className="form-label">first name</label>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="First name"
-            aria-label="First name"
-            required
-          ></input>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home 
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
         </div>
-        <div class="col">
-          <label className="form-label">last name</label>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Last name"
-            aria-label="Last name"
-            required
-          ></input>
-        </div>
-      </div>
-      <form className="row g-3">
-        <div className="col-md-6">
-          <label for="inputEmail4" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="votre e-mail"
-            id="inputEmail4"
-            required
-          ></input>
-        </div>
-        <div className="col-md-6">
-          <label for="inputPassword4" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="inputPassword4"
-            required
-          ></input>
-        </div>
-        <div className="col-12">
-          <label for="inputAddress" className="form-label">
-            Address
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputAddress"
-            placeholder="1234 Main St"
-            required
-          ></input>
-        </div>
-        <div className="col-12">
-          <label for="inputAddress2" className="form-label">
-            Address 2
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputAddress2"
-            placeholder="Apartment, studio, or floor"
-            required
-          ></input>
-        </div>
-        <div className="col-md-6">
-          <label for="inputCity" className="form-label">
-            City
-          </label>
-          <input type="text" className="form-control" id="inputCity"></input>
-        </div>
-        <div className="col-md-4">
-          <label for="inputState" className="form-label">
-            State
-          </label>
-          <select id="inputState" className="form-select">
-            <option selected>Choose...</option>
-            <option>algerie</option>
-            <option>italia</option>
-            <option>epagne</option>
-            <option>turquia</option>
-            <option>tunisia</option>
-          </select>
-        </div>
-        <div className="col-md-2">
-          <label for="inputZip" className="form-label">
-            Zip
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputZip"
-            required
-          ></input>
-        </div>
-        <div className="col-12">
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="gridCheck"
-              required
-            ></input>
-            <label className="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
-        </div>
-        <div className="col-12">
-          <button type="submit" id="button" className="btn btn-primary">
-            Sign in
-          </button>
-        </div>
-      </form>
+      </nav>
+      <p>ReactJS Form with Validation and layout</p>
+      <form class="row g-3">
+  <div class="col-md-4">
+    <label for="validationDefault01" class="form-label">First name</label>
+    <input type="text" class="form-control" id="validationDefault01"  required></input>
+  </div>
+  <div class="col-md-4">
+    <label for="validationDefault02" class="form-label">Last name</label>
+    <input type="text" class="form-control" id="validationDefault02"  required></input>
+  </div>
+  <div class="col-md-4">
+    <label for="validationDefaultUsername" class="form-label">Username</label>
+    <div class="input-group">
+      <span class="input-group-text" id="inputGroupPrepend2">@</span>
+      <input type="text" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required></input>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <label for="validationDefault03" class="form-label">City</label>
+    <input type="text" class="form-control" id="validationDefault03" required></input>
+  </div>
+  <div class="col-md-3">
+    <label for="validationDefault04" class="form-label">State</label>
+    <select class="form-select" id="validationDefault04" required>
+      <option selected disabled value="">Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+    <label for="validationDefault05" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="validationDefault05" required></input>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required></input>
+      <label class="form-check-label" for="invalidCheck2">
+        Agree to terms and conditions
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+</form>
     </div>
   );
-  
 }
 
 export default App;
